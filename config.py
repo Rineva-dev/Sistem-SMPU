01beee74864aa807f60081dcdae5b6a21bf8d725
Rineva-dev <rhyzalkurniawan97@gmai.com>
2026-09-10 13:49:26 +0800

    perbaikan pembacaan database_URL

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'kunci_default_untuk_tes_saja')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///sekolah.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
